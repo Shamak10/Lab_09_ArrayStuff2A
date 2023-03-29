5826e2bd3f284b0483e57eb99b27018bc88fa257
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-29 16:36:46 -0400

    Update Lab_09_ArrayStuff2E.java

--- src/Lab_09_ArrayStuff2E.java
+++ src/Lab_09_ArrayStuff2E.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 public class LinearScanE {
     public static void main(String[] args) {
-        int[] dataPoints = new int[100];
+        int[] dataPoints = new int[100];//created an array of 100 integers
         for (int i = 0; i < dataPoints.length; i++) {
             dataPoints[i] = (int) (Math.random() * 100) + 1;
         }
-        System.out.println("Array: " + Arrays.toString(dataPoints));
-        int minValue = dataPoints[0];
-        int maxValue = dataPoints[0];
+        System.out.println("Array: " + Arrays.toString(dataPoints));//prints the array
+        int minValue = dataPoints[0];//finds the min value
+        int maxValue = dataPoints[0];//finds the max value
         for (int i = 1; i < dataPoints.length; i++) {
             if (dataPoints[i] < minValue) {
                 minValue = dataPoints[i];
@@ -16,12 +16,12 @@ public class LinearScanE {
                 maxValue = dataPoints[i];
             }
         }
-        System.out.println("Minimum value in the array: " + minValue);
-        System.out.println("Maximum value in the array: " + maxValue);
+        System.out.println("Minimum value in the array: " + minValue);//prints out min value
+        System.out.println("Maximum value in the array: " + maxValue);//prints out max value
         double average = getAverage(dataPoints);
-        System.out.println("Average of dataPoints is: " + average);
+        System.out.println("Average of dataPoints is: " + average);//prints the average value
     }
-    public static double getAverage(int[] values) {
+    public static double getAverage(int[] values) {//calculates the average value
         double sum = 0;
         for (int i = 0; i < values.length; i++) {
             sum += values[i];
