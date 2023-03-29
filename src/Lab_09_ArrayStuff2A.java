29de7ad6b2f89aafcb566c1b56ad02150c354675
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-29 16:16:50 -0400

    Update Lab_09_ArrayStuff2A.java

--- src/Lab_09_ArrayStuff2A.java
+++ src/Lab_09_ArrayStuff2A.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Linear_Scan {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int userInput = SafeInput.getRangedInt(scanner, "Please enter an integer between 1 and 100", 1, 100);
-        System.out.println("You entered: " + userInput);
+        Scanner scanner = new Scanner(System.in);//asking the user for the input
+        int userInput = SafeInput.getRangedInt(scanner, "Please enter an integer between 1 and 100", 1, 100);// Select a number between 1 and 100
+        System.out.println("You entered: " + userInput);// prints the users input
         scanner.close();
     }
 }
