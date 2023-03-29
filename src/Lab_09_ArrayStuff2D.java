02c767e8ac99764fb555b11316cc0611ac601f6a
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-29 16:33:13 -0400

    Update Lab_09_ArrayStuff2D.java

--- src/Lab_09_ArrayStuff2D.java
+++ src/Lab_09_ArrayStuff2D.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 public class LinearScanD {
     public static void main(String[] args) {
-        int[] dataPoints = new int[100];
+        int[] dataPoints = new int[100];//create an array of 100 integers
         for (int i = 0; i < dataPoints.length; i++) {
-            dataPoints[i] = (int) (Math.random() * 100) + 1;
+            dataPoints[i] = (int) (Math.random() * 100) + 1;// choose a random number between 1 and 100
         }
-        System.out.println("Array: " + Arrays.toString(dataPoints));
-        int minValue = dataPoints[0];
-        int maxValue = dataPoints[0];
-        for (int i = 1; i < dataPoints.length; i++) {
+        System.out.println("Array: " + Arrays.toString(dataPoints));//Prints the array
+        int minValue = dataPoints[0];//looks for Min value in the array
+        int maxValue = dataPoints[0];//looks for the max value in the array
+        for (int i = 1; i < dataPoints.length; i++) {//loop for updating min and max values
             if (dataPoints[i] < minValue) {
                 minValue = dataPoints[i];
             }
@@ -16,7 +16,7 @@ public class LinearScanD {
                 maxValue = dataPoints[i];
             }
         }
-        System.out.println("Minimum value in the array: " + minValue);
-        System.out.println("Maximum value in the array: " + maxValue);
+        System.out.println("Minimum value in the array: " + minValue);//Prints min value
+        System.out.println("Maximum value in the array: " + maxValue);//Prints max value
     }
 }
