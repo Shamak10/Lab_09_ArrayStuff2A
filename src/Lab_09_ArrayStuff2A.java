92fdedcf3121acd4a750fe6e2fe96bc9ce430907
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-29 16:09:52 -0400

    Update Lab_09_ArrayStuff2A.java

--- src/Lab_09_ArrayStuff2A.java
+++ src/Lab_09_ArrayStuff2A.java
@@ -1,17 +1,10 @@
 import java.util.Scanner;
-public class SafeInput {
-    public static int getRangedInt(Scanner pipe, String prompt, int min, int max) {
-        int retInt = 0;// Declare a variable called retInt
-        do {// Uses a do-while loop to prompt the user for input
-            System.out.print("\n" + prompt + " : ");// Display the prompt to the user
-            while (!pipe.hasNextInt()) {// Uses while Loop for invalid inputs
-                System.out.print("Invalid input. " + prompt + " : ");
-                pipe.next();
-            }
-            retInt = pipe.nextInt();// Integer value used by the user
-        } while (retInt < min || retInt > max);// Checking if the integer is outside Min or Max
 
-        pipe.nextLine();
-        return retInt;
+public class Linear_Scan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userInput = SafeInput.getRangedInt(scanner, "Please enter an integer between 1 and 100", 1, 100);
+        System.out.println("You entered: " + userInput);
+        scanner.close();
     }
 }
