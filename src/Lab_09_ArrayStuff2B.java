d117aba07f723100a5457b79ae65645cf08989a6
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-29 16:01:38 -0400

    Update Lab_09_ArrayStuff2B.java

--- src/Lab_09_ArrayStuff2B.java
+++ src/Lab_09_ArrayStuff2B.java
@@ -2,19 +2,19 @@ import java.util.Random;
 import java.util.Scanner;
 public class LinearScanB {
     public static void main(String[] args) {
-        int[] dataPoints = new int[100];
+        int[] dataPoints = new int[100];// Created an array of 100 integers
         Random rand = new Random();
-        for (int i = 0; i < dataPoints.length; i++) {
+        for (int i = 0; i < dataPoints.length; i++) {//generates a random number between 1 to 100 
             dataPoints[i] = rand.nextInt(100) + 1;
         }
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);//Asking the user to select a number between 1 and 100
         int userValue = SafeInput.getRangedInt(sc, "Enter an integer between 1 and 100: ", 1, 100);
         int count = 0;
         for (int value : dataPoints) {
-            if (value == userValue) {
+            if (value == userValue) {//Counting the number of times the users number appears
                 count++;
             }
         }
-        System.out.println("The value " + userValue + " was found " + count + " times in the array.");
+        System.out.println("The value " + userValue + " was found " + count + " times in the array.");// Gives you the result
     }
 }
