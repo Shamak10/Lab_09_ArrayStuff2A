5b192356fd0ef8e813d11857fd8ee2514f6ddbdb
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-29 15:56:41 -0400

    Update Lab_09_ArrayStuff2A.java

--- src/Lab_09_ArrayStuff2A.java
+++ src/Lab_09_ArrayStuff2A.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class SafeInput {
     public static int getRangedInt(Scanner pipe, String prompt, int min, int max) {
-        int retInt = 0;
-        do {
-            System.out.print("\n" + prompt + " : ");
-            while (!pipe.hasNextInt()) {
+        int retInt = 0;// Declare a variable called retInt
+        do {// Uses a do-while loop to prompt the user for input
+            System.out.print("\n" + prompt + " : ");// Display the prompt to the user
+            while (!pipe.hasNextInt()) {// Uses while Loop for invalid inputs
                 System.out.print("Invalid input. " + prompt + " : ");
                 pipe.next();
             }
-            retInt = pipe.nextInt();
-        } while (retInt < min || retInt > max);
+            retInt = pipe.nextInt();// Integer value used by the user
+        } while (retInt < min || retInt > max);// Checking if the integer is outside Min or Max
 
-        pipe.nextLine(); // clear the input buffer
+        pipe.nextLine();
         return retInt;
     }
 }
